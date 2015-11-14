c8ec66fa124cd994bb6efffe10259fe0206cf02d
PaulVagner <prodigyzro@gmail.com>
2015-11-14 16:00:39 -0500

    parts added

--- MyTravel/imagePickerViewController.swift
+++ MyTravel/imagePickerViewController.swift
@@ -24,14 +24,5 @@ class imagePickerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
